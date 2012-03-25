0a404e75660aa255d1171ea36ce89fcdc8be81d4
Aleksandr Rakov <aleksandr.rakov@gmail.com>
2012-03-26 00:45:50 +0400

    Added some new functions

--- script.js
+++ script.js
@@ -1,40 +1,36 @@
-function genpw(id){
+function genpw(id,arg1,arg2,arg3,arg4){
     obj=document.getElementById(id);
-    obj.value=GeneratePassword();
+    arg1=document.getElementById(arg1);
+    arg2=document.getElementById(arg2);
+    arg3=document.getElementById(arg3);
+    arg4=document.getElementById(arg4);
+    obj.value=GeneratePassword(20,arg1.checked,arg2.checked,arg3.checked,arg4.checked);
 }
 
-function GeneratePassword() {
-    var length=20;
+function GeneratePassword(length,arg1,arg2,arg3,arg4) {
     var res = '';
-    var noPunction = true;
+    var str='';
+    var str1='qwertyuioplkjhgfdsazxcvbnm';
+    var str2='QWERTYUIOPLKJHGFDSAZXCVBNM';
+    var str3='1234567890';
+    var str4='!@#$%^&*.,';
+
+    if(arg1){ str=str+str1; }
+    if(arg2){ str=str+str2; }
+    if(arg3){ str=str+str3; }
+    if(arg4){ str=str+str4; }
 
     for (i=0; i < length; i++) {
-        j = getRandomNum();
-        if (noPunction) {
-    	     while (checkPunc(j)) {
-    	        j = getRandomNum(); 
-    	     }
-	 }
-        res = res + String.fromCharCode(j);
+        j = getRandomNum(str.length);
+        res = res + str.charAt(j);
     }
     return res;
 }
 
-function getRandomNum() {
+function getRandomNum(cnt) {
     // between 0 - 1
     var rndNum = Math.random()
-    // rndNum from 0 - 1000
-    rndNum = parseInt(rndNum * 1000);
-    // rndNum from 33 - 127
-    rndNum = (rndNum % 94) + 33;
+    rndNum = parseInt(rndNum * cnt);
     return rndNum;
 }
 
-function checkPunc(num) {
-
-    if ((num >=33) && (num <=47)) { return true; }
-    if ((num >=58) && (num <=64)) { return true; }
-    if ((num >=91) && (num <=96)) { return true; }
-    if ((num >=123) && (num <=126)) { return true; }
-    return false;
-}
